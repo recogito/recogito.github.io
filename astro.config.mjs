942112d57b9873c421ab64793401c46ad79dbee2
Jamie Folsom <jamiefolsom@gmail.com>
2024-05-15 10:44:34 -0400

    Upgrade to latest astro and starlight
    Adding expressive code package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,38 +1,40 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import expressiveCode from "astro-expressive-code";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://recogito.github.io/',
-	integrations: [
-		starlight({
-			title: 'Recogito v4.0.0',
-			defaultLocale: 'root', // optional
-			locales: {
-				root: {
-					label: 'English',
-					lang: 'en', // lang is required for root locales
-				},
-			},
-			logo: {
-				src: './src/assets/logo.svg',
-			},			
-			social: {
-				github: 'https://github.com/recogito/',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: 'https://recogito.github.io/',
+  integrations: [starlight({
+    title: 'Recogito v4.0.0',
+    defaultLocale: 'root',
+    // optional
+    locales: {
+      root: {
+        label: 'English',
+        lang: 'en' // lang is required for root locales
+      }
+    },
+    logo: {
+      src: './src/assets/logo.svg'
+    },
+    social: {
+      github: 'https://github.com/recogito/'
+    },
+    sidebar: [{
+      label: 'Guides',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Example Guide',
+        link: '/guides/example/'
+      }]
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }]
+  }), expressiveCode()]
 });
